5c4545bb0789ee7e705c3ed901d69f0a182653cf
Username <nguwah.kyaw@gmail.com>
2021-07-16 16:43:40 -0700

    updated work

--- controllers/api.js
+++ controllers/api.js
@@ -1,1 +1,23 @@
 import { Comment } from '../models/comment.js'
+
+export {
+    createComment,
+    commentIndex
+  }
+  
+  function commentIndex(req, res) {
+    Comment.find({})
+    .then(comments => res.json(Comments))
+    .catch(err => {
+      res.json(err)
+    })
+  }
+  
+  function createComment(req, res) {
+    Comment.create(req.body)
+    .then(comment => res.json(Comment))
+    .catch(err => {
+      console.log(err)
+      res.json(err)
+    })
+  }
