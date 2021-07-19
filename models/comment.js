1da14d193c83d7cfffe6b73355da6eb1d13ea622
Username <nguwah.kyaw@gmail.com>
2021-07-19 14:09:38 -0700

    updated controllers

--- models/comment.js
+++ models/comment.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+const Schema = mongoose.Schema
 
 export {
   Comment
@@ -7,8 +8,8 @@ export {
 const commentSchema = new mongoose.Schema({
   content: String,
   CommtextEntry: {type: String},
+  uniqque : true,
 }, {
-  timestamps: true
-})
+  timestamps: true})
 
 const Comment = mongoose.model("Comment", commentSchema)
